40de0caf4e765e4e39c2600c81c8148a2503cb41
dimasm93 <software.dimas_m@icloud.com>
2020-07-09 12:53:11 +0700

    update remove package

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Body from './container/Body'
 
 function App() {
     return (
-        <div className="App">
+        <div className="container">
+            <Body/>
+
+            <footer>
+                version: {process.env.REACT_APP_VERSION} @Copyright 2020
+            </footer>
         </div>
     );
 }
